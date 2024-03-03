1c6a9b8313a4144b4cb0f907605cadacb16db780
Zhuzishuo <zhuzishuo916@gmail.com>
2024-03-04 01:31:49 +0800

    first commit

---
+++ src/content/mockData.ts
@@ -0,0 +1,178 @@
+export const carouselImgs = [ 
+	{ 
+		src: "https://media.istockphoto.com/id/1396844697/zh/%E7%85%A7%E7%89%87/wind-power-generation-clean-energy-concept-wind-tubines-in-holland.webp?s=2048x2048&w=is&k=20&c=VIz7LHFyqIyEiRRdTI6Zss48_hpdRDuFGhlBHIjNNSo=",
+		href: "https://media.istockphoto.com/id/1396844697/zh/%E7%85%A7%E7%89%87/wind-power-generation-clean-energy-concept-wind-tubines-in-holland.webp?s=2048x2048&w=is&k=20&c=VIz7LHFyqIyEiRRdTI6Zss48_hpdRDuFGhlBHIjNNSo="
+	},
+	{
+		src: "https://media.istockphoto.com/id/1129704414/zh/%E7%85%A7%E7%89%87/%E8%8D%B7%E8%98%AD%E8%AB%BE%E5%BE%B7%E5%B8%83%E6%8B%89%E7%8F%AD%E7%89%B9%E7%9A%84-waalwijk-%E5%BE%9E%E9%B3%A5%E9%B3%A5%E7%9A%84%E8%A6%96%E9%87%8E%E4%B8%AD%E7%9C%8B%E5%88%B0%E7%9A%84%E6%B0%B4%E9%82%8A%E7%9A%84%E9%A2%A8%E8%BB%8A.webp?s=2048x2048&w=is&k=20&c=JgmkvIDwVzo51sjws_UNqH_jixm2VepKxx3HpAov_r8=",
+		href: "https://media.istockphoto.com/id/1129704414/zh/%E7%85%A7%E7%89%87/%E8%8D%B7%E8%98%AD%E8%AB%BE%E5%BE%B7%E5%B8%83%E6%8B%89%E7%8F%AD%E7%89%B9%E7%9A%84-waalwijk-%E5%BE%9E%E9%B3%A5%E9%B3%A5%E7%9A%84%E8%A6%96%E9%87%8E%E4%B8%AD%E7%9C%8B%E5%88%B0%E7%9A%84%E6%B0%B4%E9%82%8A%E7%9A%84%E9%A2%A8%E8%BB%8A.webp?s=2048x2048&w=is&k=20&c=JgmkvIDwVzo51sjws_UNqH_jixm2VepKxx3HpAov_r8="
+	}, 
+	{ 
+		src: "https://media.istockphoto.com/id/1144416985/zh/%E7%85%A7%E7%89%87/%E6%97%A5%E8%90%BD%E6%99%82%E7%9A%84%E9%A2%A8%E5%8A%9B%E6%B8%A6%E8%BC%AA%E6%A9%9F.webp?s=2048x2048&w=is&k=20&c=2GuZs-Lb6FWUFxLdttFvfSqWNC6reb93npIdxeKyEi4=",
+		href: "https://media.istockphoto.com/id/1144416985/zh/%E7%85%A7%E7%89%87/%E6%97%A5%E8%90%BD%E6%99%82%E7%9A%84%E9%A2%A8%E5%8A%9B%E6%B8%A6%E8%BC%AA%E6%A9%9F.webp?s=2048x2048&w=is&k=20&c=2GuZs-Lb6FWUFxLdttFvfSqWNC6reb93npIdxeKyEi4="
+	}
+];
+export const imgs = JSON.stringify(carouselImgs);
+
+export const cardContent = [ 
+	{ 
+		src: "https://media.istockphoto.com/id/1450272068/zh/%E7%85%A7%E7%89%87/wind-sun-and-water-energy.webp?s=2048x2048&w=is&k=20&c=RSsDZ5tbl-UBrif-35eRSU8ZDk6qwXo0N3Xt1xiRLaA=",
+		href: "https://media.istockphoto.com/id/1450272068/zh/%E7%85%A7%E7%89%87/wind-sun-and-water-energy.webp?s=2048x2048&w=is&k=20&c=RSsDZ5tbl-UBrif-35eRSU8ZDk6qwXo0N3Xt1xiRLaA=",
+		title: "基金取消净值估算，有什么影响？会不会影响基金的收益？",
+		description: "基金取消净值估算，有什么影响？会不会影响基金的收益？",
+		author: "莲花"
+	},
+	{
+		src: "https://media.istockphoto.com/id/956459500/zh/%E7%85%A7%E7%89%87/%E5%9C%A8%E6%B5%B7%E6%B4%8B%E4%B8%AD%E7%9A%84%E9%A2%A8%E8%BB%8A.webp?s=2048x2048&w=is&k=20&c=fdorBT2oGoy_Ug0b41wVgiPUMEPrTYmoT6cQxUgijLI=",
+		href: "https://media.istockphoto.com/id/956459500/zh/%E7%85%A7%E7%89%87/%E5%9C%A8%E6%B5%B7%E6%B4%8B%E4%B8%AD%E7%9A%84%E9%A2%A8%E8%BB%8A.webp?s=2048x2048&w=is&k=20&c=fdorBT2oGoy_Ug0b41wVgiPUMEPrTYmoT6cQxUgijLI=",
+		title: "在以学铸魂上下功夫见实效（专题深思）",
+		description: "钱是一块一块上万，麦是一颗一颗上石。——谚语",
+		author: "电子科技大学"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2023/10/24/03/38/animals-8337310_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2023/10/24/03/38/animals-8337310_1280.jpg",
+		title: "近期，“全世界只有中国有公摊面积”“西安女子买69平方米公寓37平方米是公摊”等“公摊面积”话题再次引起网友热议，在调研中小财发现，大部分人是同意取消“公摊面积”的。那为何多年来，该项制度还没取消？",
+		description: "青春虚度无所成，白首衔悲补何及！——权德舆",
+		author: "无问"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2024/01/18/10/50/cormorant-8516719_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2024/01/18/10/50/cormorant-8516719_1280.jpg",
+		title: "一文看懂，居民的钱都投资去了哪里？",
+		description: "一个国家里有才华的人，如同矿藏里的黄金——美国",
+		author: "探清水河"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2023/12/08/14/22/mushroom-8437670_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2023/12/08/14/22/mushroom-8437670_1280.jpg",
+		title: "停产、裁员，这家老牌合资车企撑不住了？",
+		description: "马斯克：如果没有外星人，那么我们就只是黑暗深渊中一根小蜡烛",
+		author: "荣威"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2024/01/30/21/23/desert-8542838_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2024/01/30/21/23/desert-8542838_1280.jpg",
+		title: "纽约时报 | 没有中国，世界还能造动力电池吗",
+		description: "【视评】展现青春之我，逐梦青春主场",
+		author: "这就是生活"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2024/01/29/20/33/river-8540767_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2024/01/29/20/33/river-8540767_1280.jpg",
+		title: "纽约时报 | 没有中国，世界还能造动力电池吗",
+		description: "【视评】展现青春之我，逐梦青春主场",
+		author: "Waiting For Love"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2023/05/15/14/02/cat-7995160_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2023/05/15/14/02/cat-7995160_1280.jpg",
+		title: "纽约时报 | 没有中国，世界还能造动力电池吗",
+		description: "【视评】展现青春之我，逐梦青春主场",
+		author: "山河无恙在我胸"
+	}
+];
+export const content = JSON.stringify(cardContent);
+
+export const cardContent2 = [ 
+	{ 
+		src: "https://media.istockphoto.com/id/1329405816/zh/%E7%85%A7%E7%89%87/hong-kong.jpg?s=612x612&w=0&k=20&c=cIdnnfZ9lxoyYr-xrRtFEtozLToAKq97o_bW-8mJDUg=",
+		href: "https://media.istockphoto.com/id/1329405816/zh/%E7%85%A7%E7%89%87/hong-kong.jpg?s=612x612&w=0&k=20&c=cIdnnfZ9lxoyYr-xrRtFEtozLToAKq97o_bW-8mJDUg=",
+		title: "已完成步骤已完成步骤已完成步骤已完成步骤已完成步骤已完成步骤已完成步骤已完成步骤已完成步骤",
+		author: "Jeffery"
+	},
+	{
+		src: "https://media.istockphoto.com/id/962144676/zh/%E7%85%A7%E7%89%87/%E9%A6%99%E6%B8%AF%E5%A4%A9%E9%9A%9B%E7%B7%9A%E7%99%BD%E9%9B%B2.jpg?s=612x612&w=0&k=20&c=ZdZ-r4rk32hrhnaX7KzEJhjXstCm44LQ3dhHkjBu5HE=",
+		href: "https://media.istockphoto.com/id/962144676/zh/%E7%85%A7%E7%89%87/%E9%A6%99%E6%B8%AF%E5%A4%A9%E9%9A%9B%E7%B7%9A%E7%99%BD%E9%9B%B2.jpg?s=612x612&w=0&k=20&c=ZdZ-r4rk32hrhnaX7KzEJhjXstCm44LQ3dhHkjBu5HE=",
+		title: "永源",
+		author: "mina"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2017/01/14/12/59/iceland-1979445_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2017/01/14/12/59/iceland-1979445_1280.jpg",
+		title: "angelcare（守护宝）",
+		author: "赚钱养心雨hh"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2020/01/26/20/14/computer-4795762_1280.jpg",
+		title: "手机市场又崩了！下个季度回暖？",
+		author: "微甜汽水"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2017/12/10/15/16/white-horse-3010129_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2017/12/10/15/16/white-horse-3010129_1280.jpg",
+		title: "论坛论道丨肖钢：大力发展数字经济",
+		author: "八分钟的温暖"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2017/08/15/09/30/office-2643259_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2017/08/15/09/30/office-2643259_1280.jpg",
+		title: "今天我们终于失去了一切",
+		author: "W. F. Y"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2016/03/19/04/04/ecology-1266172_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2016/03/19/04/04/ecology-1266172_1280.jpg",
+		title: "汇元世纪城",
+		author: "中大国际文化有限公司"
+	}, 
+	{ 
+		src: "https://cdn.pixabay.com/photo/2015/05/31/11/23/mobile-791164_1280.jpg",
+		href: "https://cdn.pixabay.com/photo/2015/05/31/11/23/mobile-791164_1280.jpg",
+		title: "Don t Leave Me Alone",
+		author: "法信公司"
+	}, 
+	{ 
+		src: "https://media.istockphoto.com/id/927002010/zh/%E7%85%A7%E7%89%87/%E5%9C%B0%E9%BB%9E-dolomiti-%E9%98%BF%E7%88%BE%E5%8D%91%E6%96%AF-%E8%85%8E%E4%B8%8A%E8%85%BA%E7%9A%AE%E8%B3%AA%E6%BF%80%E7%B4%A0-d-ampezzo-%E5%B8%95%E8%98%87%E8%B1%90%E6%9D%9C-di-%E9%98%AE%E6%96%87%E9%A5%92-%E5%A8%81%E5%B0%BC%E6%89%98-%E7%BE%A9%E5%A4%A7%E5%88%A9-%E6%AD%90%E6%B4%B2.jpg?s=612x612&w=0&k=20&c=zcqzFZeRHhvNQbXeisvt2DiGLS7OzuiWuXBYqduZsg0=",
+		href: "https://media.istockphoto.com/id/927002010/zh/%E7%85%A7%E7%89%87/%E5%9C%B0%E9%BB%9E-dolomiti-%E9%98%BF%E7%88%BE%E5%8D%91%E6%96%AF-%E8%85%8E%E4%B8%8A%E8%85%BA%E7%9A%AE%E8%B3%AA%E6%BF%80%E7%B4%A0-d-ampezzo-%E5%B8%95%E8%98%87%E8%B1%90%E6%9D%9C-di-%E9%98%AE%E6%96%87%E9%A5%92-%E5%A8%81%E5%B0%BC%E6%89%98-%E7%BE%A9%E5%A4%A7%E5%88%A9-%E6%AD%90%E6%B4%B2.jpg?s=612x612&w=0&k=20&c=zcqzFZeRHhvNQbXeisvt2DiGLS7OzuiWuXBYqduZsg0=",
+		title: "青海省海东市互助土族故土园景区",
+		author: "两百斤土豆"
+	}, 
+	{ 
+		src: "https://media.istockphoto.com/id/1253016563/zh/%E7%85%A7%E7%89%87/%E8%B2%BB%E5%BE%B7%E6%8B%89%E6%B9%96%E5%9C%A8%E7%A7%8B%E5%A4%A9%E7%9A%84%E6%A3%AE%E6%9E%97%E8%88%87%E7%B4%85%E6%9D%89-%E5%A4%9A%E6%B4%9B%E7%B1%B3%E8%92%82-%E7%BE%A9%E5%A4%A7%E5%88%A9.jpg?s=612x612&w=0&k=20&c=pKC62JjpH-FQqVZ5WUpXZdKzlfqHYMI_ZqlS9JyV_L8=",
+		href: "https://media.istockphoto.com/id/1253016563/zh/%E7%85%A7%E7%89%87/%E8%B2%BB%E5%BE%B7%E6%8B%89%E6%B9%96%E5%9C%A8%E7%A7%8B%E5%A4%A9%E7%9A%84%E6%A3%AE%E6%9E%97%E8%88%87%E7%B4%85%E6%9D%89-%E5%A4%9A%E6%B4%9B%E7%B1%B3%E8%92%82-%E7%BE%A9%E5%A4%A7%E5%88%A9.jpg?s=612x612&w=0&k=20&c=pKC62JjpH-FQqVZ5WUpXZdKzlfqHYMI_ZqlS9JyV_L8=",
+		title: "新疆自治区巴音州和静巴音布鲁克景区",
+		author: "能吃一缸"
+	}, 
+	{ 
+		src: "https://media.istockphoto.com/id/496068538/zh/%E7%85%A7%E7%89%87/beautiful-pond-of-swiss-alps-in-autumn.jpg?s=612x612&w=0&k=20&c=DspGGz2n0Z5tWgjvSdv1nuL85k8PJyMuWAXMtNuEkRw=",
+		href: "https://media.istockphoto.com/id/496068538/zh/%E7%85%A7%E7%89%87/beautiful-pond-of-swiss-alps-in-autumn.jpg?s=612x612&w=0&k=20&c=DspGGz2n0Z5tWgjvSdv1nuL85k8PJyMuWAXMtNuEkRw=",
+		title: "亚洲最大的洞穴腾龙洞，里面可升降直升飞机，长达60公里",
+		author: "Dosence"
+	}
+];
+export const content2 = JSON.stringify(cardContent2);
+
+export const cardContent3 = [ 
+	{ 
+		href: "https://media.istockphoto.com/id/1144416985/zh/%E7%85%A7%E7%89%87/%E6%97%A5%E8%90%BD%E6%99%82%E7%9A%84%E9%A2%A8%E5%8A%9B%E6%B8%A6%E8%BC%AA%E6%A9%9F.webp?s=2048x2048&w=is&k=20&c=2GuZs-Lb6FWUFxLdttFvfSqWNC6reb93npIdxeKyEi4=",
+		title: "进北大每人交10800元，研学还是打劫？"
+	},
+	{
+		href: "https://media.istockphoto.com/id/1253016563/zh/%E7%85%A7%E7%89%87/%E8%B2%BB%E5%BE%B7%E6%8B%89%E6%B9%96%E5%9C%A8%E7%A7%8B%E5%A4%A9%E7%9A%84%E6%A3%AE%E6%9E%97%E8%88%87%E7%B4%85%E6%9D%89-%E5%A4%9A%E6%B4%9B%E7%B1%B3%E8%92%82-%E7%BE%A9%E5%A4%A7%E5%88%A9.jpg?s=612x612&w=0&k=20&c=pKC62JjpH-FQqVZ5WUpXZdKzlfqHYMI_ZqlS9JyV_L8=",
+		title: "水果不甜也会高糖？山楂到底会不会让人长胖？"
+	}, 
+	{ 
+		href: "https://media.istockphoto.com/id/962144676/zh/%E7%85%A7%E7%89%87/%E9%A6%99%E6%B8%AF%E5%A4%A9%E9%9A%9B%E7%B7%9A%E7%99%BD%E9%9B%B2.jpg?s=612x612&w=0&k=20&c=ZdZ-r4rk32hrhnaX7KzEJhjXstCm44LQ3dhHkjBu5HE=",
+		title: "前些时候网红名师的一席话，是撕开了皇帝的新装，还是纯属外行凑热闹？这似乎是个见仁见智的问题。可这两天“张雪峰效应”的席卷全网，以及“张雪峰受害者”和“捡漏幸运儿”的不断涌现，又将人拉回到争议的原点——新闻，真的不值得学了？"
+	}, 
+	{ 
+		href: "https://media.istockphoto.com/id/1396844697/zh/%E7%85%A7%E7%89%87/wind-power-generation-clean-energy-concept-wind-tubines-in-holland.webp?s=2048x2048&w=is&k=20&c=VIz7LHFyqIyEiRRdTI6Zss48_hpdRDuFGhlBHIjNNSo=",
+		title: "比一个人吃火锅更寂寞的是，一个人，没钱吃火锅。"
+	}, 
+	{ 
+		href: "https://media.istockphoto.com/id/496068538/zh/%E7%85%A7%E7%89%87/beautiful-pond-of-swiss-alps-in-autumn.jpg?s=612x612&w=0&k=20&c=DspGGz2n0Z5tWgjvSdv1nuL85k8PJyMuWAXMtNuEkRw=",
+		title: "比一个人吃火锅更寂寞的是，一个人，没钱吃火锅。"
+	}
+];
+export const content3 = JSON.stringify(cardContent3);
+
+
+
+
+export const pageInfo = {
+	size: 3,
+	pages: 1,
+	current: 1
+}
